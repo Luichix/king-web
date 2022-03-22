d8ffe3e318c605871bac5267d0f221d8b15d55f4
Luichix <luigui.rex@gmail.com>
2022-03-21 21:11:16 -0600

    implementacion de firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyC-AxmjEyoAvrs0I-OjQnGvCf-H7aiYfkc',
+  authDomain: 'king-client.firebaseapp.com',
+  projectId: 'king-client',
+  storageBucket: 'king-client.appspot.com',
+  messagingSenderId: '456285720702',
+  appId: '1:456285720702:web:8b8e7a9878059075e1c82f',
+  measurementId: 'G-150V7Y5FV2'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+
+export { analytics }
